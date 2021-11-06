32dbd47f51971b1a2025da0516f6f0ca58e160f8
Rozlyn <65274371+RozlynTr@users.noreply.github.com>
2021-11-06 11:11:11 -0600

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const copyrightyr = document.getElementById('copyrightyr');
+copyrightyr.textContent = new Date().getFullYear();
+
+const lastupdate = document.lastModified;
+document.getElementById('lastupdate').textContent = lastupdate;
