ff8056e121bcfc9318257f96c0fad135a376dde6
Rozlyn <65274371+RozlynTr@users.noreply.github.com>
2021-12-08 19:36:27 -0700

    Create test.js

---
+++ lesson-06/js/test.js
@@ -0,0 +1,10 @@
+const currenttown = window.location.href;
+const sodasprings = 'http://127.0.0.1:5500/lesson-06/sodasprings.html';
+const preston = 'http://127.0.0.1:5500/lesson5/preston-10.html';
+const fishhaven = 'http://127.0.0.1:5500/lesson-06/fishhaven.html';
+//const type = typeof(sodasprings);
+//console.log(type);
+if (currenttown === sodasprings || currenttown === preston || currenttown === fishhaven) {
+  console.log(currenttown);
+  
+};
